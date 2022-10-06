ec173f2b8b729b054a721ba405bf59d50f111bf6
Eric Turner <eric@ericturner.dev>
2022-10-06 17:51:51 -0400

    Make particles work in three dimensions

--- wispy-lines/src/wispy_lines/particle.clj
+++ wispy-lines/src/wispy_lines/particle.clj
@@ -7,33 +7,38 @@
 (defn create-particle
   ([] (create-particle 0 0 0))
   ([x y z] (create-particle x y z 0 0 0))
-  ([x y z vx vy vz] (create-particle x y z vx vy z 0 0 0))
+  ([x y z vx vy vz] (create-particle x y z vx vy vz 0 0 0))
   ([x y z vx vy vz ax ay az] {:pos      [x y z]
-                      :prev-pos [x y z]
-                      :vel      [vx vy vz]
-                      :acc      [ax ay az]}))
+                              :prev-pos [x y z]
+                              :vel      [vx vy vz]
+                              :acc      [ax ay az]}))
 
-(defn- adjust-position [pos max-w max-h]
+(defn- adjust-position
   "Adjusts the position to stay within the boundaries"
-  (let [x (mod (nth pos 0) max-w)
-        y (mod (nth pos 1) max-h)
-        z (nth pos 2)]
-    [x y z]))
+  ([pos max-x max-y] (adjust-position pos max-x max-y 1))
+  ([pos max-x max-y max-z]
+   (let [x (mod (nth pos 0) max-x)
+         y (mod (nth pos 1) max-y)
+         z (mod (nth pos 2) max-z)]
+     [x y z])))
 
-(defn- adjust-previous-position [prev-pos curr-pos max-x max-y]
-  "Adjusts the previous position so it isn't all the way across the view area when the current position crosses the area boundaries"
-  (let [curr-x (nth curr-pos 0)
-        curr-y (nth curr-pos 1)
-        curr-z (nth curr-pos 2)
-        adj-curr-pos (adjust-position curr-pos max-x max-y)
-        adj-curr-x (nth adj-curr-pos 0)
-        adj-curr-y (nth adj-curr-pos 1)
-        prev-x (nth prev-pos 0)
-        prev-y (nth prev-pos 1)
-        x (if (or (>= curr-x max-x) (< curr-x 0)) adj-curr-x prev-x)
-        y (if (or (>= curr-y max-y) (< curr-y 0)) adj-curr-y prev-y)
-        z curr-z]
-    [x y z]))
+(defn- adjust-previous-position
+  ([prev-pos curr-pos max-x max-y] (adjust-previous-position prev-pos curr-pos max-x max-y 1))
+  ([prev-pos curr-pos max-x max-y max-z]
+   (let [curr-x (nth curr-pos 0)
+         curr-y (nth curr-pos 1)
+         curr-z (nth curr-pos 2)
+         adj-curr-pos (adjust-position curr-pos max-x max-y max-z)
+         adj-curr-x (nth adj-curr-pos 0)
+         adj-curr-y (nth adj-curr-pos 1)
+         adj-curr-z (nth adj-curr-pos 2)
+         prev-x (nth prev-pos 0)
+         prev-y (nth prev-pos 1)
+         prev-z (nth prev-pos 2)
+         x (if (or (>= curr-x max-x) (< curr-x 0)) adj-curr-x prev-x)
+         y (if (or (>= curr-y max-y) (< curr-y 0)) adj-curr-y prev-y)
+         z (if (or (>= curr-z max-z) (< curr-z 0)) adj-curr-z prev-z)]
+     [x y z])))
 
 (defn apply-force [frc particle]
   (let [acc (m/add (:acc particle) frc)]
