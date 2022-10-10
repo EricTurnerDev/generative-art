8267d88c07ce771eee684920756a98421bd2d33a
Eric Turner <eric@ericturner.dev>
2022-10-10 15:32:43 -0400

    Tweak signature location

--- rushing-water/src/rushing_water/core.clj
+++ rushing-water/src/rushing_water/core.clj
@@ -8,10 +8,12 @@
 (def ^:const image-height 1500)
 (def ^:const step-length (* image-width 0.001))
 (def ^:const line-iterations 10000)
+(def ^:const draw-border? true)
 (def ^:const draw-flow-field? false)
+(def ^:const sign? true)
 (def ^:const border-thickness 60)                           ; Should be no less than 50
 (def ^:constant seed (rand-int (Integer/MAX_VALUE)))
-;(def ^:constant seed 0)                                     ; Specify a seed to re-create an image
+;(def ^:constant seed 0)                                    ; Specify a seed to re-create an image
 
 (def ^:const left-x 0)
 (def ^:const right-x image-width)
@@ -80,38 +82,42 @@
 
 (defn draw-curve
   "Draws a curve through a flow field"
-  ([start-x start-y num-steps flow-field]
-   (draw-curve start-x start-y num-steps flow-field 3))
-  ([start-x start-y num-steps flow-field weight]
+  ([color start-x start-y num-steps flow-field]
+   (draw-curve color start-x start-y num-steps flow-field 3))
+  ([color start-x start-y num-steps flow-field weight]
    (let [points (curve-points start-x start-y num-steps flow-field)]
+     (q/stroke-weight weight)
      (doseq [[x y _] points]
+       (apply q/stroke color)
        (q/point x y)))))
 
 (defn draw-border
   ([color] (draw-border color 100))
   ([color thickness]
    (apply q/stroke color)
-   (q/rect 0 0 image-width thickness)                ; Top
+   (q/rect 0 0 image-width thickness)                       ; Top
    (q/rect (- image-width thickness) 0 thickness image-height) ; Right
    (q/rect 0 (- image-height thickness) image-width thickness) ; Bottom
-   (q/rect 0 0 thickness image-height)               ; Left
+   (q/rect 0 0 thickness image-height)                      ; Left
    ))
 
 (defn sign [author date seed border-thickness]
   "Adds a signature to the bottom of the image"
   (q/fill 0 0 0)
   (let [text (format "%s, %s" author date)
-        x (- image-width border-thickness 130)
+        x (- image-width border-thickness 132)
         y (+ (- image-height border-thickness) 20)]
     (q/text text x y))
-  (let [x (- image-width border-thickness 130)
+  (let [x (- image-width border-thickness 132)
         y (+ (- image-height border-thickness) 40)]
     (q/text seed x y)))
 
 (defn blueish []
+  "Create a random blueish/greenish color"
   [0 (q/random 100 200) (q/random 200 256) (q/random 100)])
 
 (defn reddish []
+  "Create a random reddish/greenish color"
   [(q/random 256) (q/random 256) 0 (q/random 256)])
 
 (defn draw-state [state]
@@ -120,25 +126,34 @@
     (apply q/background background-color)
 
     (dotimes [_ line-iterations]
-      (q/stroke-weight (q/random 1 15))
-      (apply q/stroke (blueish))
-      (draw-curve (q/random 0 image-width) (q/random 0 image-height) (q/random 1000 15000) (:flow-field state)))
+      (let [color (blueish)
+            weight (q/random 1 15)
+            x (q/random image-width)
+            y (q/random image-height)
+            steps (q/random 1000 15000)]
+        (draw-curve color x y steps (:flow-field state) weight))
+      )
 
     (dotimes [_ (int (* line-iterations 0.02))]
-      (q/stroke-weight (q/random 1 5))
-      (apply q/stroke (reddish))
-      (draw-curve (q/random 0 image-width) (q/random 0 image-height) (q/random 1000 15000) (:flow-field state)))
+      (let [color (reddish)
+            weight (q/random 1 5)
+            x (q/random 0 image-width)
+            y (q/random 0 image-height)
+            steps (q/random 1000 15000)]
+        (draw-curve color x y steps (:flow-field state) weight)))
 
     (when draw-flow-field?
       (draw-flow-field (:flow-field state) (:resolution state)))
 
-    (draw-border [255 255 255] border-thickness)
+    (when draw-border?
+      (draw-border [255 255 255] border-thickness))
 
-    (sign
-      "Eric Turner"
-      (.format (java.text.SimpleDateFormat. "MM/dd/yyyy")(java.util.Date.))
-      (format "%s" seed)
-      border-thickness)
+    (when sign?
+      (sign
+        "Eric Turner"
+        (.format (java.text.SimpleDateFormat. "MM/dd/yyyy") (java.util.Date.))
+        (format "%s" seed)
+        border-thickness))
 
     (q/save "rushing-water.png")
 
