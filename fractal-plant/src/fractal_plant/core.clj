01a03ae41c6e1600a0c3d14de039b4af3134e9a1
Eric Turner <eric@ericturner.dev>
2022-10-15 11:47:55 -0400

    Make fractal leaves functional (i.e. use Quil fun-mode middleware)

--- fractal-plant/src/fractal_plant/core.clj
+++ fractal-plant/src/fractal_plant/core.clj
@@ -1,8 +1,8 @@
 (ns fractal-plant.core
   (:require [quil.core :as q]
+            [quil.middleware :as m]
             [clojure.string :as string]))
 
-(def ^:const num-generations 8)
 (def ^:const width 800)
 (def ^:const height 800)
 (def ^:const angle (q/radians 25))
@@ -16,55 +16,59 @@
                                \- "-"
                                \[ "["
                                \] "]"})
-(def word (atom "X"))
-(def draw-rules (atom {}))
+(def ^:const draw-rules {
+                         \F (fn []
+                              (let [len (* -1 length)]
+                                (apply q/stroke stem-color)
+                                (q/line 0 0 0 len)
+                                (q/translate 0 len)))
+                         \X (fn [])
+                         \+ (fn []
+                              (q/rotate (* -1 angle)))
+                         \- (fn []
+                              (q/rotate angle))
+                         \[ q/push-matrix
+                         \] (fn []
+                              (q/no-stroke)
+                              (apply q/fill leaf-color)
+                              (q/ellipse 0 0 (* 2 length) (* 5 length))
+                              (q/pop-matrix))
+                         })
 
 (defn setup []
   (q/frame-rate 30)
-  (q/stroke-weight 1)
-  (reset! draw-rules {
-                      \F (fn []
-                           (let [len (* -1 length)]
-                             (apply q/stroke stem-color)
-                             (q/line 0 0 0 len)
-                             (q/translate 0 len)))
-                      \X (fn [] )
-                      \+ (fn []
-                           (q/rotate (* -1 angle)))
-                      \- (fn []
-                           (q/rotate angle))
-                      \[ q/push-matrix
-                      \] (fn []
-                           (q/no-stroke)
-                           (apply q/fill leaf-color)
-                           (q/ellipse 0 0 (* 2 length) (* 5 length))
-                           (q/pop-matrix))
-                      })
-  (q/no-loop))
+  {:word "X"})
 
 (defn generate [wrd]
   (string/join (map production-rules wrd)))
 
 (defn draw-generation [wrd]
   (apply q/background background-color)
-  ;; We don't want transformations and rotations from previous generations to affect future
-  ;; generations, so push the current transformation matrix, draw this generation, and pop it
-  ;; back off to restore the original.
+  ;; Use push-matrix and pop-matrix to prevent transformations in the current generation from
+  ;; affecting future generations.
   (q/push-matrix)
-  (dorun (map #((@draw-rules %)) wrd))
+  (dorun (map #((draw-rules %)) wrd))
   (q/pop-matrix))
 
-(defn draw []
+(defn update-state [state]
+  {:word (generate (:word state))})
+
+(defn draw-state [state]
   (q/with-translation [(* width 0.2) (* height 0.9)]        ; Start in the bottom left
-    (q/with-rotation [angle]
-      (dotimes [_ num-generations]
-        (draw-generation @word)
-        (reset! word (generate @word)))))
-  (q/save "fractal-plant.png"))
+                      (q/with-rotation [angle]
+                                       (draw-generation (:word state))))
+  (q/no-loop))
+
+(defn on-mouse-press [state _]
+  (q/start-loop)
+  state)
 
 (q/defsketch fractal-plant
              :title "Fractal Plant"
              :size [width height]
              :setup setup
-             :draw draw
-             :features [:keep-on-top])
+             :update update-state
+             :draw draw-state
+             :features [:keep-on-top]
+             :mouse-pressed on-mouse-press
+             :middleware [m/fun-mode])
