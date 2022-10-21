9fc786a9ebe70d6dc75cc1b4f457a74a65644d9a
Eric Turner <eric@ericturner.dev>
2022-10-21 13:12:41 -0400

    Add line-drawing

---
+++ line-drawing/README.md
@@ -0,0 +1,3 @@
+# Line Drawing
+
+Following [this thread on Twitter](https://twitter.com/generativelight/status/1519759169045381127).
---
+++ line-drawing/src/line_drawing/core.clj
@@ -0,0 +1,28 @@
+(ns line-drawing.core
+  (:require [quil.core :as q]))
+
+(def ^:const antiquewhite [34 14 98])
+
+(defn mouse-pressed [event]
+  (q/save "outputSave.png"))
+
+(defn setup []
+  (q/frame-rate 30)
+  (q/color-mode :hsb 360 100 100)
+  (q/no-loop))
+
+(defn draw []
+  (apply q/background antiquewhite)
+  (q/stroke-cap :square)
+  (dotimes [_ 5]
+    (q/stroke-weight (q/random 50))
+    (q/stroke (q/random 50) (q/random 100) (q/random 100))
+    (q/line (q/random 50 350) (q/random 50 350) (q/random 50 350) (q/random 50 350))))
+
+(q/defsketch line-drawing
+             :title "Line Drawing"
+             :size [400 400]
+             :setup setup
+             :draw draw
+             :features [:keep-on-top]
+             :mouse-pressed mouse-pressed)
